7745f1f966a216732ae332d24a9b577eb1214f73
Gabriel <gabriel.wrowe@gmail.com>
2017-06-26 11:48:53 -0400

    fix: typo in gitignore

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ app.use(express.static(path.join(__dirname, './path/to/your/static/assets')))
 // Any routes or other various middlewares should go here!
 
 
+
+
+
 // Make sure this is right at the end of your server logic!
 // The only thing after this might be a piece of middleware to serve up 500 errors for server problems
 // (However, if you have middleware to serve up 404s, that go would before this as well)
