d1aace85bf876eef86360b5bc844e13ca1c72cf3
Gabriel <gabriel.wrowe@gmail.com>
2017-08-11 12:49:43 -0400

    update: provider

--- client/index.js
+++ client/index.js
@@ -1,9 +1,19 @@
-import { Provider } from 'react-redux';
+
+//React Imports
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { render } from 'react-dom'
+import { connect, Provider } from 'react-redux'
 import store from './store.js';
+import './public/scss/index.scss' //imports scss into browser files
 
 ReactDOM.render(
-  <Provider store={store}>
-    {/* rest of your app goes here! */}
+  <Provider store={ store }>
+    <Router path="/" component={ Root } >
+      <Router path="/calculator" component={ calculator } />
+      <Router path="/about" component={ about } />
+      <IndexRoute component={ Home } />
+    </Router>
+    <Route path='*' component={ NotFound } />
   </Provider>,
   document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: {}
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
       }
     ]
   }
